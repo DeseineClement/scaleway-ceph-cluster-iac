534744ab4b07c8cf925d7677d6d8180aa53a7e90
Korbo <clement.deseine@gmail.com>
2023-09-29 18:21:23 +0200

    fix(IaC): used image_id instead of name

--- modules/scaleway_instances/main.tf
+++ modules/scaleway_instances/main.tf
@@ -10,6 +10,6 @@ resource "scaleway_instance_server" "servers" {
     count = var.instance_count
     name = 1 == count.index ? var.instance_name : "${var.instance_name}_${count.index}"
     type = var.instance_type
-    image = data.scaleway_instance_image.my_image.name
+    image = data.scaleway_instance_image.my_image.image_id
     ip_id = scaleway_instance_ip.instance_ip[count.index].id
 }

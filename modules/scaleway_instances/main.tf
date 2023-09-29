50624510a59e3c48fe6f4d3661c586b3ec99efe7
Korbo <clement.deseine@gmail.com>
2023-09-29 18:18:26 +0200

    fix(IaC): fixed ternary condition

--- modules/scaleway_instances/main.tf
+++ modules/scaleway_instances/main.tf
@@ -8,7 +8,7 @@ resource "scaleway_instance_ip" "instance_ip" {
 
 resource "scaleway_instance_server" "servers" {
     count = var.instance_count
-    name = count.index ? var.instance_name : "${var.instance_name}_${count.index}"
+    name = 1 == count.index ? var.instance_name : "${var.instance_name}_${count.index}"
     type = var.instance_type
     image = data.scaleway_instance_image.my_image.name
     ip_id = scaleway_instance_ip.instance_ip[count.index].id

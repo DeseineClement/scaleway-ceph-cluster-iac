9f7d6e5a6794a1bb65c4d38adbc8f686c948296f
Korbo <clement.deseine@gmail.com>
2023-10-01 11:17:53 +0200

    feat(CD): added validate and tflint

--- modules/scaleway_instances/terraform.tf
+++ modules/scaleway_instances/terraform.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     scaleway = {
-      source = "scaleway/scaleway"
+      source = "scaleway/scaleway",
+      version = "~> 2.28"
     }
   }
   required_version = ">= 0.13"
--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     scaleway = {
       source = "scaleway/scaleway"
+      version = "~> 2.28"
     }
   }
   required_version = ">= 0.13"

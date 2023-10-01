f895a4a3035262767d26260010e183fdf28ba574
Korbo <clement.deseine@gmail.com>
2023-10-01 11:34:37 +0200

    feat(IaC): used dict mapped by instance name as outputs

--- modules/scaleway_instances/main.tf
+++ modules/scaleway_instances/main.tf
@@ -8,7 +8,7 @@ resource "scaleway_instance_ip" "instance_ip" {
 
 resource "scaleway_instance_server" "servers" {
   count = var.instance_count
-  name  = 1 == var.instance_count ? var.instance_name : "${var.instance_name}_${count.index}"
+  name  = 1 == var.instance_count ? var.instance_name : "${var.instance_name}_${count.index + 1}"
   type  = var.instance_type
   image = data.scaleway_instance_image.my_image.image_id
   ip_id = scaleway_instance_ip.instance_ip[count.index].id
--- modules/scaleway_instances/output.tf
+++ modules/scaleway_instances/output.tf
@@ -1,9 +1,15 @@
 output "instance_public_ips" {
   description = "The public Ip addresses allocated to each VMs"
-  value       = [for server in scaleway_instance_server.servers : server.public_ip]
+  value = {
+    for index, server in scaleway_instance_server.servers :
+    (1 == var.instance_count ? var.instance_name : "${var.instance_name}_${index + 1}") => server.public_ip
+  }
 }
 
 output "instance_private_ips" {
-  description = "The public Ip addresses allocated to each VMs"
-  value       = [for server in scaleway_instance_server.servers : server.private_ip]
+  description = "The private Ip addresses allocated to each VMs"
+  value = {
+    for index, server in scaleway_instance_server.servers :
+    (1 == var.instance_count ? var.instance_name : "${var.instance_name}_${index + 1}") => server.private_ip
+  }
 }
